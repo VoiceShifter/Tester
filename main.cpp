97088edf0e7cac260c65134b71f103926a7d3d5c
VoiceShifter <gdy2003@yandex.ru>
2024-12-14 06:51:46 -0800

    feat: test for second microservice

--- main.cpp
+++ main.cpp
@@ -38,17 +38,53 @@ std::string AddSubscription(std::string Username, std::string Password)
     return Query.str();
 }
 
+std::string AuthorizeArtist (std::string Username, std::string Password)
+{
+    std::stringstream Query{};
+    Query << "GET /Api/Artist/?Email=" << Username << "&Password=" << Password;
+    return Query.str();
+
+}
+
+std::string AddArtist (std::string Username, std::string Password, std::string PhoneNumber)
+{
+    std::stringstream Query{};
+    Query << "PUT /Api/Artist/?Email=" << Username << "&Password=" << Password
+          << "&PhoneNumber=" << PhoneNumber;
+    return Query.str();
+}
+
+std::string DeleteArtist (std::string Username, std::string Password)
+{
+    std::stringstream Query{};
+    Query << "POST /Api/Artist/?Email=" << Username << "&Password=" << Password;
+    return Query.str();
+}
+
+std::string PendingRequest(std::string Username, std::string Album, std::string Name)
+{
+    std::stringstream Query{};
+    Query << "POST /Api/Artist/Releases/?Email=" << Username
+          << "&Album=" << Album << "&Name=" << Name;
+    std::cout << Query.str();
+    return Query.str();
+}
+
+
 int main(int argc, char *argv[])
 {
 
 
     QTcpSocket MainSocket{};
-    MainSocket.bind(32323);
-    MainSocket.connectToHost(QHostAddress::LocalHost, 32323);
+    MainSocket.bind(32324); //323234 - artist 32323 - users
+    MainSocket.connectToHost(QHostAddress::LocalHost, 32324);
     MainSocket.waitForConnected(-1);
     //QByteArray Message{AddUser("Username9", "1", "1").c_str()};
     //QByteArray Message{DeleteUser("Username9", "1").c_str()}; //AddUser("Username9", "1", "123").c_str()
-    QByteArray Message{AddSubscription("Username9", "1").c_str()};
+    //QByteArray Message{AddSubscription("Username9", "1").c_str()};
+    //QByteArray Message{AddArtist("Username9", "1", "123").c_str()}; //AddUser("Username9", "1", "123").c_str()
+    //QByteArray Message{DeleteArtist("Username9", "1").c_str()};
+    QByteArray Message{PendingRequest("Username9", "Demon_Days", "Feels_good_inc").c_str()};
     MainSocket.write(Message);
     MainSocket.flush();
     MainSocket.waitForBytesWritten();
